b53b47a10c11ff52e6903fba8115d2dffac68c3d
Richard D'Alves <54530477+richarddalves@users.noreply.github.com>
2025-04-10 11:44:24 -0300

    Pontuação finalizada para todos os grupos

--- src/cenas/mini-games/celular/InterfaceCelularTres.js
+++ src/cenas/mini-games/celular/InterfaceCelularTres.js
@@ -110,7 +110,7 @@ export default class InterfaceCelularTres extends Phaser.Scene {
       .setInteractive()
       .on("pointerdown", () => {
         InterfaceCelularTres.gruposDeletados.add(this.grupoAtual);
-        this.hud.alterarPontuacao(10);
+        this.hud.alterarPontuacao(4);
         this.hud.esconder();
         this.scene.start("JogoCelular");
       });
@@ -142,7 +142,7 @@ export default class InterfaceCelularTres extends Phaser.Scene {
 
         InterfaceCelularTres.mensagensDeletadas.add(eoxisKey);
 
-        this.hud.alterarPontuacao(5);
+        this.hud.alterarPontuacao(4);
         this.mostrarFeedback(true, eoxisKey);
       });
   }
